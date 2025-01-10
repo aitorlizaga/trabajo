f80812712134f49b40624cad1afdb3fa4ee83a5c
aitorlizaga <aa20.alizaga16@iespabloserrano.com>
2025-01-10 14:13:39 +0100

    Delete bloque1 directory

--- bloque1/eliminarprod.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_REQUEST['producto'])) {
-    $producto = $_REQUEST['producto'];
-        unset($_SESSION["producto"][$producto]);
-
-}
-header('Location: ejemplo10.php'); 
-?>
